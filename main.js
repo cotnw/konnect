4c95928706dbd476ff6f27e40f9ba6a21cb310ed
Vedanta Somnathe <vedusom142005@gmail.com>
2021-09-17 12:05:50 +0530

    setup

---
+++ main.js
@@ -0,0 +1,40 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const dbURI = process.env.DB_URL;
+const Grid = require("gridfs-stream");
+
+// DB CONNECTION
+async function connectDB() {
+  await mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+}
+connectDB();
+
+// GRIDFS SETTINGS
+const conn = mongoose.connection;
+let gfs;
+conn.once("open", () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("fs");
+});
+
+// SETTINGS
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(__dirname + "/public"));
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
