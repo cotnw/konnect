ae5536c3fb0a1c166c2186618c727132eb22a530
Laxya Pahuja <laxya.pahuja8@gmail.com>
2021-09-18 23:51:06 +0530

    profile

--- routes/index.js
+++ routes/index.js
@@ -2,54 +2,62 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 
-router.get("/register", async (req, res) => {
-  res.render("register");
-});
-
-router.get("/registered", async (req, res) => {
-  res.render("registered");
-});
-
-router.post("/register", async (req, res) => {
-  let user = await User.findOne({ access_token: req.body.access_token });
-  if (user) {
-    user.skills = req.body.skills;
-    user.title = req.body.title;
-    user.portfolio = req.body.portfolio;
-    user.about = req.body.about;
-    user.contact_email = req.body.contact_email;
-    await user.save();
-    res.json({ success: true, message: "Register successful." });
-  } else {
-    res.json({ success: false, message: "Access Token invalid." });
-  }
-});
-
 router.get("/", (req, res) => {
-  res.render("dashboard");
+    res.render("index");
+});
+
+router.get("/register", async(req, res) => {
+    res.render("register");
+});
+
+router.get("/registered", async(req, res) => {
+    res.render("registered");
+});
+
+router.post("/register", async(req, res) => {
+    let user = await User.findOne({ access_token: req.body.access_token });
+    if (user) {
+        user.skills = req.body.skills;
+        user.title = req.body.title;
+        user.portfolio = req.body.portfolio;
+        user.about = req.body.about;
+        user.contact_email = req.body.contact_email;
+        await user.save();
+        res.json({ success: true, message: "Register successful." });
+    } else {
+        res.json({ success: false, message: "Access Token invalid." });
+    }
+});
+
+router.get("/dashboard", (req, res) => {
+    res.render("dashboard");
 });
 
 router.get("/create", (req, res) => {
-  res.render("createProject");
+    res.render("createProject");
 });
 
 router.post("/create", (req, res) => {
-  // CODE HERE
+    // CODE HERE
 });
 
 router.get("/project", (req, res) => {
-  res.render("project");
+    res.render("project");
 });
 
-async function checkAuth(req, res, next) {
-  const accessToken = req.query.accessToken;
-  const user = await User.findOne({ access_token: accessToken });
+router.get("/profile", (req, res) => {
+    res.render("profile")
+})
 
-  if (user) {
-    next();
-  } else {
-    res.redirect("/err");
-  }
+async function checkAuth(req, res, next) {
+    const accessToken = req.query.accessToken;
+    const user = await User.findOne({ access_token: accessToken });
+
+    if (user) {
+        next();
+    } else {
+        res.redirect("/err");
+    }
 }
 
 module.exports = router;
