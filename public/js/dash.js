91876aead6fa58d25e993bd7bc8fe7f0b3fc7cde
Vedanta Somnathe <vedusom142005@gmail.com>
2021-09-19 12:03:26 +0530

    dash search

--- public/js/dash.js
+++ public/js/dash.js
@@ -5,6 +5,10 @@ for (let card of document.querySelectorAll(".project")) {
     let h2 = card.children[1].textContent.toLowerCase();
     titles.push(h2);
 }
+for (let card of document.querySelectorAll(".user")) {
+    let h2 = card.children[1].textContent.toLowerCase();
+    titles.push(h2);
+}
 
 search.addEventListener("keyup", (e) => {
     let usrInp = search.value.trim();
@@ -13,15 +17,40 @@ search.addEventListener("keyup", (e) => {
         for (let card of document.querySelectorAll(".project")) {
             card.classList.remove("hidden");
         }
+        for (let card of document.querySelectorAll(".user")) {
+            card.classList.remove("hidden");
+        }
+
+        document.querySelector(".main h1").textContent =
+            "Project matches for you";
+
+        document.querySelector(".switch-btns").classList.add("hidden");
+
+        document.querySelector("users").classList.add("hidden");
     } else {
         for (let card of document.querySelectorAll(".project")) {
             card.classList.add("hidden");
         }
+        for (let card of document.querySelectorAll(".user")) {
+            card.classList.add("hidden");
+        }
+
+        document.querySelector(".switch-btns").classList.remove("hidden");
+
+        document.querySelector(
+            ".main h1"
+        ).textContent = `Search results for "${usrInp}"`;
     }
     for (let match of matches) {
         for (let title of titles) {
             if (match == title) {
                 for (let card of document.querySelectorAll(".project")) {
+                    if (card.children[1].textContent.toLowerCase() == title) {
+                        card.classList.remove("hidden");
+                    }
+                }
+
+                for (let card of document.querySelectorAll(".user")) {
                     if (card.children[1].textContent.toLowerCase() == title) {
                         card.classList.remove("hidden");
                     }
@@ -41,4 +70,22 @@ function getMatches(input) {
     }
 
     return matchList;
+}
+
+for (let btn of document.querySelectorAll(".switch-btns .btn")) {
+    btn.addEventListener("click", () => {
+        if (btn.textContent.trim() === "People") {
+            document.querySelector(".users").classList.remove("hidden");
+            document.querySelector(".projects").classList.add("hidden");
+        } else {
+            document.querySelector(".users").classList.add("hidden");
+            document.querySelector(".projects").classList.remove("hidden");
+        }
+
+        document
+            .querySelector(".switch-btn-active")
+            .classList.remove("switch-btn-active");
+
+        btn.classList.add("switch-btn-active");
+    });
 }
